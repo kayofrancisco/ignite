b76b9208435c55addeb2fe56a8e6d0e3699ef7e4
Kayo Francisco Silva Bezerra <kayofrancisco@hotmail.com>
2021-06-10 06:18:22 -0300

    Revert "Adicionado pasta para separar node e react"
    This reverts commit 5b6e0503aaf90433607bd9026ee82dc12b8dd382.

---
+++ 04-introducao-solid/src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.params;
+
+    return response.json(this.turnUserAdminUseCase.execute({ user_id }));
+  }
+}
+
+export { TurnUserAdminController };
